1a89ff6809135ab2f3976d7e34afc51f30b0d2c0
NGUYEN PHI NHAN <phinhannguyen04@gmail.com>
2024-11-10 23:25:41 +0700

    Test mail sending

--- app/Http/Controllers/SanbayController.php
+++ app/Http/Controllers/SanbayController.php
@@ -3,17 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sanbay;
+use App\Mail\MailClass;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class SanbayController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    public static function sendEmail() { 
+        
+        $details = [ 'title' => 'Mail from Laravel', 'body' => 'This is a test email using Laravel.' ]; 
+        
+        Mail::to('phinhannguyen04@gmail.com')->send(new MailClass($details)); 
+        
+        return "Email sent successfully!";
+        
+    }
+    
     public function index()
     {
         //
-        $sanbays = Sanbay::all();
-        return view('galaxy.index', compact('sanbays'));
+        $mess =self::sendEmail();
+        // $sanbays = Sanbay::all();
+        // return view('galaxy.index', compact('sanbays'));
+        return $mess;
     }
 }
